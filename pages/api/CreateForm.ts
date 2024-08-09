561f542ce3a7f6d3a0ceaa98a1897279add5b368
Vijay Reddy <vijay.in.vnr@gmail.com>
2024-08-10 00:44:01 +0530

    removed un necessary line
    error.message is causing an syntax error. we can deal this later but for now I have commented this out.

--- pages/api/CreateForm.ts
+++ pages/api/CreateForm.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             res.status(200).json({ formId });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: error.message });
+            //res.status(500).json({ error: error.message });
         }
     } else {
         res.setHeader('Allow', ['POST']);
