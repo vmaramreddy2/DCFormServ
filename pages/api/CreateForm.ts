321bb99015f325bfbfc2ff59f21900a03c00fc3e
Vijay Reddy <vijay.in.vnr@gmail.com>
2024-08-10 00:48:22 +0530

    Update CreateForm.ts

--- pages/api/CreateForm.ts
+++ pages/api/CreateForm.ts
@@ -4,14 +4,14 @@ import { CreateForm } from '../../actions/form';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        try {
-            const formData = req.body;
-            const formId = await CreateForm(formData);
-            res.status(200).json({ formId });
-        } catch (error) {
-            console.error(error);
-            //res.status(500).json({ error: error.message });
-        }
+        // try {
+        //     const formData = req.body;
+        //     const formId = await CreateForm(formData);
+        //     res.status(200).json({ formId });
+        // } catch (error) {
+        //     console.error(error);
+        //     //res.status(500).json({ error: error.message });
+        // }
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
